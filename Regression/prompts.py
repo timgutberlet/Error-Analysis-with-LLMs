130873ba09774f3946c6fd8cf768b93e64c6c5b9
Tim Gutberlet <t.gutberlet01@gmail.com>
2024-02-22 21:01:45 +0100

    #4 Regression Pipelines Completed
    #4 Regression ML Models Completed
    
    #2 Sentiment Analysis progress

--- Regression/prompts.py
+++ Regression/prompts.py
@@ -24,3 +24,36 @@ Required Output:
 
 Please provide the prediction strictly adhering to the above instructions.""")
 
+few_shot = PromptTemplate(
+input_variables=[],
+template=
+"""Example 1:
+{example_1}
+
+Example 2:
+{example_2}
+
+Example 3:
+{example_3}
+
+System: Based on the provided features of a used car listed below, please predict its selling price in Indian Rupees in the Indian market. The predicted price should be expressed solely as a number followed by the currency "INR".
+Ensure that the output contains no additional text or characters beyond this specified format.
+Features:
+name: {name},
+year: {year},
+km_driven: {km_driven},
+fuel: {fuel},
+seller_type: {seller_type},
+transmission: {transmission},
+owner: {owner},
+mileage: {mileage},
+engine: {engine},
+max_power: {max_power},
+torque: {torque},
+seats: {seats}
+
+Required Output:
+"price": <predicted price> INR
+
+Please provide the prediction strictly adhering to the above instructions.""")
+
